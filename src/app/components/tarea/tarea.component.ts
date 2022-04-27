7e9d648e194cf5f0039b018a1b609d60be200cb5
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-04-27 18:07:51 -0300

    Tutorial completo

---
+++ src/app/components/tarea/tarea.component.ts
@@ -0,0 +1,40 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html',
+  styleUrls: ['./tarea.component.css'],
+})
+export class TareaComponent implements OnInit {
+  todo: string[] = ['Hacer la comida', 'Lavar los platos', 'Lavar el auto'];
+  progress: string[] = [];
+  done: string[] = [];
+  tarea = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  agregarTarea() {
+    if (this.tarea.trim() === '') {
+      this.tarea = '';
+      return;
+    }
+    this.todo.push(this.tarea);
+    this.tarea = '';
+  }
+}
